08614b32ccb7b7dc97c43d29816494becddb992b
HaozheWang <haozhe.wang.chn@outlook.com>
2026-03-25 00:44:59 -0400

    [HQQQ] Local & Docker Set Up

---
+++ src/hqqq-ui/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HQQQ iNAV Engine</h1>
+      <p>Real-time ETF fair value calculation</p>
+    </div>
+  );
+}
+
+export default App;
